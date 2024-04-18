893281e8919acb632795e9c3ee047c4401a48390
“JarlM06” <jarl.ebbesen@outlook.com>
2024-04-18 13:23:11 +0200

    Initial commit

---
+++ src/main/java/com/example/kortprogram/KortProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.kortprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KortProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KortProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kortprogram/KortProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kortprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KortProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
