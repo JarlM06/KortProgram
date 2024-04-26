48b4ebea75630278765b789b15e1fd354a0c42df
“JarlM06” <jarl.ebbesen@outlook.com>
2024-04-26 13:09:47 +0200

    Kommentarer

--- src/main/java/com/example/kortprogram/addCard.php
+++ src/main/java/com/example/kortprogram/addCard.php
@@ -1,4 +1,5 @@
 <?php
+// Koble til MariaDB-serveren
 $conn = new mysqli("localhost", "root", "Akademiet99", "Kortstokker");
 
 // Sjekk tilkoblingen
@@ -6,10 +7,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Hent innsendte verdier fra skjemaet
 $userId = $_POST['userIdInput'];
 $cardType = $_POST['cardType'];
 $cardNumber = $_POST['cardNumber'];
 
+// Utfør spørring for å legge til et nytt objekt i databasen
 $result = $conn->query("INSERT INTO stock_$userId (Type, Number) VALUES ('$cardType', '$cardNumber')");
 
 // Lukk tilkoblingen
